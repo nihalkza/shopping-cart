a43a7ebd4ce713a466d4144c9f9189fc825f54c3
Abhishek <59018927+nihalkza@users.noreply.github.com>
2021-09-07 23:36:03 +0530

    Add files via upload

---
+++ precache-manifest.a91549218548746ba9f603f3a8bc2d64.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a5775d7099cf7143d183741d966982",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "26afe593f58a1df582c6",
+    "url": "/shopping-cart/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "4a3561c22bb24e54257d",
+    "url": "/shopping-cart/static/css/main.9f901fa6.chunk.css"
+  },
+  {
+    "revision": "26afe593f58a1df582c6",
+    "url": "/shopping-cart/static/js/4.f4244037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/4.f4244037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d73c3561a983fcda66",
+    "url": "/shopping-cart/static/js/5.dbe4ced8.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/shopping-cart/static/js/5.dbe4ced8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1e7576ee4f981ccb77",
+    "url": "/shopping-cart/static/js/6.a1912ff3.chunk.js"
+  },
+  {
+    "revision": "4a3561c22bb24e54257d",
+    "url": "/shopping-cart/static/js/main.6db8b76e.chunk.js"
+  },
+  {
+    "revision": "79098ee3465bdaf3e1d8",
+    "url": "/shopping-cart/static/js/runtime-main.01fd905c.js"
+  },
+  {
+    "revision": "dcb283be28604fa49b00",
+    "url": "/shopping-cart/static/js/stencil-h8k-logo_2-entry-js.91b3f4dd.chunk.js"
+  },
+  {
+    "revision": "8e1ab548c4a73a0aab4a",
+    "url": "/shopping-cart/static/js/stencil-polyfills-css-shim.db56049d.chunk.js"
+  }
+]);
